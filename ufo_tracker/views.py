3de308aafa1fa5b621dd438fa8b7178ac503f578
Ryuzaki101 <hirako.taisho@gmail.com>
2023-10-30 11:48:57 +0100

    change of request print

--- ufo_tracker/views.py
+++ ufo_tracker/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 def hello(request):
     return HttpResponse("Hello, UFO Enthusiast!")
 def mama(request):
-    return HttpResponse("UR MAMA IS FAT!")
+    return HttpResponse("test mama request!")
